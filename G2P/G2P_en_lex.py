979c0e9fea0df3df0672cce0378e14a3828043d5
NikiPshg <nicitacom2017@gmail.com>
2024-07-26 21:37:37 +0300

    fix bugs

--- G2P/G2P_en_lex.py
+++ G2P/G2P_en_lex.py
@@ -23,7 +23,8 @@ class G2P_en_lexicon:
     def check_punctuation(self, word):
         return any(char in string.punctuation for char in word)
 
-    def __call__(self, seq_list, with_stress=True):
+    def __call__(self, seq, with_stress=True):
+        seq_list = preprocess_text(seq)
         result = []
         count_from_dict = 0
         count_from_model = 0
@@ -59,11 +60,23 @@ G2P_en_lexicon = G2P_en_lexicon(g2p=G2P,
                                 sp=SP)
 
 if __name__ == '__main__':
-    while True:
-        print('Ввод текса')
-        text_ = str(input())
-        start_time = time.time()
-        preprocess_seq = preprocess_text(text_)
-        print(G2P_en_lexicon(preprocess_seq, with_stress=False))
-        end_time = time.time()
-        print(f"{(end_time - start_time) * 1000} мc -- за это время программа была выполнена ")
+    text = """The dominant sequence transduction models are based on complex recurrent or
+            convolutional neural networks that include an encoder and a decoder. The best
+            performing models also connect the encoder and decoder through an attention
+            mechanism. We propose a new simple network architecture, the Transformer,
+            based solely on attention mechanisms, dispensing with recurrence and convolutions
+            entirely. Experiments on two machine translation tasks show these models to
+            be superior in quality while being more parallelizable and requiring significantly
+            less time to train. Our model achieves 28.4 BLEU on the WMT 2014 Englishto-German translation task,
+            improving over the existing best results, including
+            ensembles, by over 2 BLEU. On the WMT 2014 English-to-French translation task,
+            our model establishes a new single-model state-of-the-art BLEU score of 41.8 after
+            training for 3.5 days on eight GPUs, a small fraction of the training costs of the
+            best models from the literature. We show that the Transformer generalizes well to
+            other tasks by applying it successfully to English constituency parsing both with
+            large and limited training data."""
+
+    start_time = time.time()
+    print(G2P_en_lexicon(text, with_stress=True))
+    end_time = time.time()
+    print(f"{(end_time - start_time) * 1000} мc -- за это время программа была выполнена ")
